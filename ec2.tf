84b14e5b67b55ea6c6cdae12ce7ff7c1b0197e31
yusuf1207 <mukhammadturgunov@gmail.com>
2023-02-24 02:27:36 +0000

    instance profile removed

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,6 @@ resource "aws_instance" "example" {
   key_name                    = "mackey"
   subnet_id                   = "subnet-0ab2fb103e79db61b"
   aws_security_group_id       = ["sg-0e14f59a31df8d27a"]
-  iam_instance_profile        = aws_iam_instance_profile.instance_profile.name
   associate_public_ip_address = true
   tags = {
     Name = "test1"
