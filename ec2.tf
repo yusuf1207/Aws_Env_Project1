80b65af72799c02ddb8c90ecc175468027de2c18
yusuf1207 <mukhammadturgunov@gmail.com>
2023-02-24 02:36:22 +0000

    testing

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "test1" {
   ami                    = data.aws_ami.amazon_linux_2.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.main.id]
   key_name               = var.key_name
   tags = {
     Name = "${var.env}-instance"                                # dev-instance, qa-instance, prod-instance
