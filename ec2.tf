f68a6564e53f7608bc40fb17233e318db0837429
yusuf1207 <mukhammadturgunov@gmail.com>
2023-02-24 02:29:22 +0000

    testing

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "example" {
   instance_type               = "t2.micro"
   key_name                    = "mackey"
   subnet_id                   = "subnet-0ab2fb103e79db61b"
-  aws_security_group_id       = ["sg-0e14f59a31df8d27a"]
+  aws_security_group_id       = ["sg-039f5dcd7d53d3ee5"]
   associate_public_ip_address = true
   tags = {
     Name = "test1"
